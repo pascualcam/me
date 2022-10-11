bd95d54fa23af26a9af4899bbef83c8899ef35c2
Pascual <pascualc02@gmail.com>
2022-10-11 16:34:55 -0500

    updated main commit to website
    includes html and css

---
+++ flights.py
@@ -0,0 +1,46 @@
+"""
+File: flights.py
+Author: Pascual Eduardo Camacho Legarreta
+Date: 10/01/22
+Description: this program computes the total cost of travel.
+
+Input:
+    - Flight cost (float)
+    - Uber cost (float)
+    - Override flag (int)
+
+Output:
+    - Total cost (float)
+    - Cost breakdown (list)
+
+The user can override the default variables by using the input override flag.
+"""
+
+# Import libraries
+import math
+
+
+# Function: get_user_input
+# Description:
+#    this function gets the user input deatailed in the description of this file
+def get_user_input():
+    return
+
+# Function: compute_cost
+# Description:
+#    ...
+def compute_cost():
+    return
+
+# Function: print_total_cost
+# Description:
+#    ...
+def print_total_cost():
+    return
+
+
+# Main function
+def main():
+    get_user_input()
+    compute_cost()
+    print_total_cost()
